749e2cea75f0c1463020344e404c6cba159597cb
priyanka <tiwary.priyanka92Z@gmail.com>
2024-02-12 15:09:14 +0100

    made the rows and columns more customizable and scalable

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   
   return (
      <div className={styles.rootStyles}>
-       <WordleProvider columns ={6}>
-       <WordleHome rows={3} columns={6}/>
+       <WordleProvider columns ={5}>
+       <WordleHome rows={5} columns={5}/>
        </WordleProvider>
       
      </div>
